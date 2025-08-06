37ffde9c62f3c1ce7abaaab4ebaa70f48e2c12d0
akankshalad84 <akankshalad9@gmail.com>
2025-08-06 18:46:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package code.com;
+
+public class Main {
+  public static void main(String args[]) {
+//	  Pizza basePizza = new Pizza(false);
+//	  basePizza.addExtraCheese();
+//	  basePizza.addExtraTopping();
+//	  basePizza.TakeAway();
+//	  basePizza.getBill();
+	  
+	  DeluxePizza dp = new DeluxePizza(false);
+	  dp.getBill();
+  }
+}
